c3a41d1f2422f7ab60f022fa7a99108a4043c8a3
gba-3 <gbayru3@gmail.com>
2022-08-16 16:22:02 +0900

    printテスト実装

--- logger_test.go
+++ logger_test.go
@@ -1,6 +1,8 @@
 package gologger
 
 import (
+	"encoding/json"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -17,11 +19,66 @@ func TestSetLogger(t *testing.T) {
 				Levels:   []string{"ERROR", "WARNING", "INFO", "DEBUG"},
 			},
 		},
+		{
+			"INFO",
+			Config{
+				MinLevel: "INFO",
+				Levels:   []string{"ERROR", "WARNING", "INFO"},
+			},
+		},
+		{
+			"WARNING",
+			Config{
+				MinLevel: "WARNING",
+				Levels:   []string{"ERROR", "WARNING"},
+			},
+		},
+		{
+			"ERROR",
+			Config{
+				MinLevel: "ERROR",
+				Levels:   []string{"ERROR"},
+			},
+		},
+		{
+			"TEST",
+			Config{
+				MinLevel: "TEST",
+				Levels:   []string{"ERROR", "WARNING", "INFO", "DEBUG"},
+			},
+		},
 	}
 	for _, testCase := range testCases {
 		SetLogger(testCase.level)
 		if !reflect.DeepEqual(testCase.expect, logger) {
 			t.Fatalf("unexpected result. expect=%v actual=%v\n", testCase.expect, logger)
 		}
+	}
+}
+
+func TestPrint(t *testing.T) {
+	t.Helper()
+	backup := os.Stdout
+	defer func() {
+		os.Stderr = backup
+	}()
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("fail pipe: %v", err)
+	}
+	os.Stdout = w
+
+	SetLogger("DEBUG")
+	print("INFO", "info message")
+	w.Close()
+
+	log := Logger{}
+	if err := json.NewDecoder(r).Decode(&log); err != nil {
+		t.Fatalf("fail decode json: %v", err)
+	}
+
+	if log.Msg != "info message" {
+		t.Fatalf("unexpected result. expect=%s actual=%s", "info message", log.Msg)
 	}
 }
